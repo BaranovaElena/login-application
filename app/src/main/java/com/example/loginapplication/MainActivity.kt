8c8057db4446bc65b57e620609fc56e411fb92c0
BaranovaElena <baranovaeaa@yandex.ru>
2021-09-28 14:30:22 +0200

    hw1: верстка экрана логина

--- app/src/main/java/com/example/loginapplication/MainActivity.kt
+++ app/src/main/java/com/example/loginapplication/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.loginapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.loginapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
